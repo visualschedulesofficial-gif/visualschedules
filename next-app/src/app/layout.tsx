1d5e809e2e7608c95745df77afe85cff8591b516
visualschedulesofficial-gif <visualschedulesofficial@gmail.com>
2026-07-02 08:56:03 +0530

    Update layout.tsx

--- next-app/src/app/layout.tsx
+++ next-app/src/app/layout.tsx
@@ -21,6 +21,18 @@ export const metadata: Metadata = {
     "ASD",
     "ADHD",
   ],
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [{ url: "/apple-touch-icon.png" }],
+    other: [
+      { url: "/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
+      { url: "/android-chrome-512x512.png", sizes: "512x512", type: "image/png" },
+    ],
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
